0166911c20d0a75c763593798ea2ef1fcc8da3e0
Craig Rupp <craigrupp@utexas.edu>
2016-11-11 18:02:13 -0600

    Prompt/Alert/Data Types

--- Refresher/script.js
+++ Refresher/script.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var name = "John";
-var lastName = "Murphy"
+var lastName = prompt("What is your last name?");
 
-var fullAge = 26
-console.log(name + " " + lastName + " is a " + fullAge + " year old wanker" );
+var fullAge = 26;
+
+var isMarried = false;
+console.log(name + " " + lastName + " is a " + fullAge + " year old wanker." + " Is he married, " + isMarried + ".");
+
+alert("Hello " + name + " " + lastName);
