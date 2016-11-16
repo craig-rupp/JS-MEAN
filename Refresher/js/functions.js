a7fd6b4fe35bd516c303d064977411815d11f55f
Craig Rupp <craigrupp@utexas.edu>
2016-11-16 15:17:34 -0600

    JS Array refresher

--- Refresher/js/functions.js
+++ Refresher/js/functions.js
@@ -24,3 +24,35 @@ yearsUntilRetirement('Dad', 1961);
 yearsUntilRetirement('Mom', 1963);
 yearsUntilRetirement('Colleen', 1987);
 yearsUntilRetirement('Janice', 1935);
+
+
+var names = ['Steve', 'Lisa', 'Craig'];
+var years = new Array(1963, 1961, 1990);
+
+console.log(names[2]);
+console.log(years[1]);
+names[1] = 'Janice';
+console.log(names);
+
+var craig = ['Craig', 'Rupp', 1990, 'velociraptor', false];
+
+craig.unshift('Mr.');
+craig.pop();
+console.log(craig);
+craig.push('blue');
+console.log(craig.indexOf(1990));
+craig.shift();
+console.log(craig);
+
+console.log(craig.indexOf(1990));
+
+if(craig.indexOf('velociraptor') === -1){
+	console.log('Craig is not a dinosaur');
+	// === -1 is the result of indexOf should the element not be maintained in the array
+} else {
+	console.log('Craig is a velociraptor');
+}
+
+
+
+
