be9eae0520d41fa269a4ebbdacc88ba46a669820
Craig Rupp <craigrupp@utexas.edu>
2016-11-16 10:50:49 -0600

    Operator Precedence

--- Refresher/script.js
+++ Refresher/script.js
@@ -9,3 +9,23 @@ var isMarried = false;
 console.log(name + " " + lastName + " is a " + fullAge + " year old wanker." + " Is he married, " + isMarried + ".");
 
 alert("Hello " + name + " " + lastName);
+
+var now = 2016;
+var birthyear = now - 1990;
+
+console.log(birthyear);
+
+
+// var ageCraig = (11 + 2) * 2;
+// console.log(ageCraig);
+
+var ageJon = 30;
+var ageTim = 30;
+ageJon = ageTim = (3 + 5) * 4 - 6;
+console.log(ageJon);
+
+ageJon++;
+console.log(ageJon);
+ageTim *= 3;
+console.log(ageTim);
+console.log( 26 * 3);
