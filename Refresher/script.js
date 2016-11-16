717e4130b7b5d84541cd8a3dcdea476a04ce2a8b
Craig Rupp <craigrupp@utexas.edu>
2016-11-16 11:04:21 -0600

    Type coersion

--- Refresher/script.js
+++ Refresher/script.js
@@ -1,31 +1,86 @@
 'use strict';
 
-var name = "John";
-var lastName = prompt("What is your last name?");
+// var name = "John";
+// var lastName = prompt("What is your last name?");
 
-var fullAge = 26;
+// var fullAge = 26;
 
-var isMarried = false;
-console.log(name + " " + lastName + " is a " + fullAge + " year old wanker." + " Is he married, " + isMarried + ".");
+// var isMarried = false;
+// console.log(name + " " + lastName + " is a " + fullAge + " year old wanker." + " Is he married, " + isMarried + ".");
 
-alert("Hello " + name + " " + lastName);
+// alert("Hello " + name + " " + lastName);
 
-var now = 2016;
-var birthyear = now - 1990;
+// var now = 2016;
+// var birthyear = now - 1990;
 
-console.log(birthyear);
+// console.log(birthyear);
 
 
-// var ageCraig = (11 + 2) * 2;
-// console.log(ageCraig);
+// // var ageCraig = (11 + 2) * 2;
+// // console.log(ageCraig);
 
-var ageJon = 30;
-var ageTim = 30;
-ageJon = ageTim = (3 + 5) * 4 - 6;
-console.log(ageJon);
+// var ageJon = 30;
+// var ageTim = 30;
+// ageJon = ageTim = (3 + 5) * 4 - 6;
+// console.log(ageJon);
 
-ageJon++;
-console.log(ageJon);
-ageTim *= 3;
-console.log(ageTim);
-console.log( 26 * 3);
+// ageJon++;
+// console.log(ageJon);
+// ageTim *= 3;
+// console.log(ageTim);
+// console.log( 26 * 3);
+
+var name = 'Craig';
+var age = 26;
+var isMarried = 'yes';
+
+if(isMarried === 'yes'){
+	console.log(name + ' is married at the young age of' + age);
+} else {
+	console.log(name + ' has made a wise decision to not be handcuffed at ' + age + ' years old');
+}
+
+isMarried = false;
+if(isMarried){
+	console.log(name + " decided to get married");
+} else {
+	console.log( name + " is not married");
+}
+
+if (23 == '23'){
+	console.log("Type Coersion worked");
+} else {
+	console.log("it did not");
+}
+
+if (23 === '23'){
+	console.log("Type Coersion worked");
+} {
+	console.log("it did not");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
