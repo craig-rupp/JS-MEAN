c89969296cfdf3bc3e48a075f68148064fae29b1
Craig Rupp <craigrupp@utexas.edu>
2016-11-23 09:26:01 -0600

    Init game function with event Listener

--- Refresher/js/app.js
+++ Refresher/js/app.js
@@ -12,9 +12,7 @@ console.log("Hello");
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init()
 
 //hide dice image on page load with css style property display
 document.querySelector('.dice').style.display = 'none';
@@ -87,6 +85,28 @@ function nextPlayer (){
 		//document.querySelector('.player-1-panel').classList.add('active');
 
 		document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+	scores = [0, 0];
+	activePlayer = 0;
+	roundScore = 0;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
 }
 
 
