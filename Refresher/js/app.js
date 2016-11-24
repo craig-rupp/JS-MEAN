1d2097b8da2f17c5893079e9d4105529d2cb1519
Craig Rupp <craigrupp@utexas.edu>
2016-11-24 08:34:04 -0600

    Added gamePlaying variable to limit dice rolling ability on a player winning

--- Refresher/js/app.js
+++ Refresher/js/app.js
@@ -10,64 +10,51 @@ GAME RULES:
 */
 console.log("Hello");
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
+gamePlaying = true;
+init();
 
-init()
-
-//hide dice image on page load with css style property display
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-
-//annonymous function can't be re-used
+//listerner for dice roll
 document.querySelector('.btn-roll').addEventListener('click', function btn(){
-	
-	// 1. Random Number
-	var dice = Math.floor(Math.random() * 6) + 1;
-	console.log(dice);
+	if (gamePlaying) {
+		// 1. Random Number
+		var dice = Math.floor(Math.random() * 6) + 1;
+		console.log(dice);
 
-	// 2. Display Result
-	var diceDom = document.querySelector('.dice');
-	diceDom.style.display = 'block';
-	diceDom.src = 'dice-' + dice + '.png';
+		// 2. Display Result
+		var diceDom = document.querySelector('.dice');
+		diceDom.style.display = 'block';
+		diceDom.src = 'dice-' + dice + '.png';
 
-	// 3. Update the Round Score If the rolled number was not a 1
-	if (dice !== 1){
-		//add score
-		roundScore += dice
-		document.querySelector('#current-' + activePlayer).textContent = roundScore;
-	} else {
-		nextPlayer();
-	}
+		// 3. Update the Round Score If the rolled number was not a 1
+		if (dice !== 1){
+			//add score
+			roundScore += dice;
+			document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		} else {
+			nextPlayer();
+		}	
+	};
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-	//alert('Hold button pressed');
-
-	//add current score to GLOBAL SCORE
-	scores[activePlayer] += roundScore;
-	console.log(scores);
-	//Update the UI
-	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-	//check if player won the game
-	if(scores[activePlayer] >= 20) {
-		//document.getElementById('name-' + activePlayer).innerHTML = '<em>Winner</em>';
-		document.getElementById('name-' + activePlayer).textContent = 'Winner!';
-		document.querySelector('.dice').style.display = 'none';
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-	} else {
-		//Change player
-		nextPlayer();	
+	if(gamePlaying){
+		scores[activePlayer] += roundScore;
+		console.log(scores);
+		//Update the UI
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+		//check if player won the game
+		if(scores[activePlayer] >= 20) {
+			//document.getElementById('name-' + activePlayer).innerHTML = '<em>Winner</em>';
+			document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		} else {
+			nextPlayer();	
+		}	
 	}
-
-
-
 });
 
 function nextPlayer (){
@@ -93,6 +80,7 @@ function init(){
 	scores = [0, 0];
 	activePlayer = 0;
 	roundScore = 0;
+	console.log(gamePlaying);
 
 	document.querySelector('.dice').style.display = 'none';
 
