71f9d8bc879543e68a1237e38436aac214e1626c
Craig Rupp <craigrupp@utexas.edu>
2016-11-28 13:19:30 -0600

    Object.create prototype building

--- Refresher/js/script_objects.js
+++ Refresher/js/script_objects.js
@@ -3,7 +3,7 @@
 console.log('Let\'s work on objects!');
 
 //Function Constructor
-
+/*
 var John = {
 	name : 'John',
 	yearOfBirth : 1990,
@@ -31,3 +31,26 @@ var Lisa = new Person('Lisa', 1963, 'CCN');
 
 Lisa.calculateAge();
 console.log(Lisa.lastName);
+*/
+//Object.create
+
+var personProto = {
+	calculateAge : function (){
+		console.log(2016 - this.yearOfBirth);
+	}
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = 'Developer';
+console.log(john);
+
+var Lisa = Object.create(personProto, {
+	name : {value : 'Lisa'},
+	yearOfBirth : {value : 1963},
+	job : {value : 'CCN'}
+});
+
+
+
