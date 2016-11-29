8c53d1804d45ccc6d262d0cb1365b3ccf0592a7f
Craig Rupp <craigrupp@utexas.edu>
2016-11-29 10:05:20 -0600

    Closures/Functions calling functions

--- Refresher/js/script_objets2.js
+++ Refresher/js/script_objets2.js
@@ -8,15 +8,15 @@ console.log('Second go round with Objects');
 function intervewQuestion(job){
 	if(job === 'designer'){
 		return function (name){
-			console.log("What does UX mean " + name + '?');
+			//console.log("What does UX mean " + name + '?');
 		}
 	} else if (job === 'director'){
 		return function (name) {
-			console.log(name + " what is your favorite film?");
+			//console.log(name + " what is your favorite film?");
 		}
 	} else {
 		return function (name) {
-			console.log("What do you do, " + name + "?");
+			//console.log("What do you do, " + name + "?");
 		}
 	}
 }
@@ -32,10 +32,52 @@ intervewQuestion('actor')('Ernesto');
 
 (function () {
 	var score = Math.random() * 10;
-	console.log(score >= 4);
+	//console.log(score >= 4);
 })();
 
 (function (goodLuck) {
 	var score = Math.random() * 10;
-	console.log(score >= 5 - goodLuck);
+	//console.log(score >= 5 - goodLuck);
 })(3);
+
+//Closures
+
+function retirement(retirementAge){
+	var a = " years left until retirement.";
+	return function(yearOfBirth){
+		var age = 2016 - yearOfBirth;
+		console.log((retirementAge - age) + a);
+	}
+}
+var retirementUs = retirement(66);
+retirementUs(1990);
+//retirement(66)(1990); same way as calling above
+
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+retirementGermany(1990);
+retirementIceland(1990); 
+
+
+function intQuestion(job){
+	var a = " Do you like to style in CSS";
+	var b = " What would you do to become an actor in Hollywood,"
+	return function(name){
+		if(job === 'designer'){
+			console.log(name + " what is your favorite webstyle design?" + a + ", " + name + "?");
+		} else if (job === 'Actor') {
+			console.log("What type of genre do you prefer acting in " + name + '?' + b + " " + name + "?");
+		}
+	}
+}
+intQuestion('designer')('Craig');
+var Actor = intQuestion('Actor');
+Actor('Tayo');
+
+
+
+
+
+
+
+
