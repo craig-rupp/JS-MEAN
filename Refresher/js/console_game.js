849ec7eb7a30200b53abec53a1b010949f95e23f
Craig Rupp <craigrupp@utexas.edu>
2016-11-30 12:25:59 -0600

    more game functionality, next question called to keep game going, score accumulated through function score added to checkAnswer function

--- Refresher/js/console_game.js
+++ Refresher/js/console_game.js
@@ -17,13 +17,23 @@
 		}
 	}
 	Question.prototype.checkAnswer = 
-	function (answer)
+	function (answer, callback)
 	{
+		var sc;
 		if(answer === this.correct){
 			 console.log('Good Job, you got it right');
+			 sc = callback(true);
 		} else {
 			console.log('Wrong Answer, try again');
+			sc = callback(false);
 		}
+		this.displayScore(sc);
+	}
+	Question.prototype.displayScore = 
+	function (score)
+	{
+		console.log("Your current score is " + score);
+		console.log("----------------- \n");
 	}
 	var first_question = new Question('Is Payet better than Ozil?', 
 		['yes', 'no'], 1);
@@ -35,14 +45,34 @@
 
 	var allQuestions = [first_question, second_question, third_question];
 
-	var random = Math.floor(Math.random() * allQuestions.length);
-	//console.log(random);
-	//console.log(allQuestions.length);
-	allQuestions[random].displayQuestion();
-	//console.log(allQuestions[random]);
+	function score() {
+		var scr = 0;
+		return function(correct){
+			if(correct){
+				scr++;
+			}
+			return scr;
+		}
+	}
+	var keepScore = score();
 
-	var answer = parseInt(prompt('Please select the correct answer.'));
-	allQuestions[random].checkAnswer(answer);
+	function nextQuestion(){
+
+		var random = Math.floor(Math.random() * allQuestions.length);
+		//console.log(random);
+		//console.log(allQuestions.length);
+		allQuestions[random].displayQuestion();
+		//console.log(allQuestions[random]);
+
+		var answer = prompt('Please select the correct answer. If you would not like to answer please type : exit');
+
+		if(answer !== 'exit'){
+			allQuestions[random].checkAnswer(parseInt(answer), keepScore);
+			nextQuestion();				
+		}
+	}
+	nextQuestion();
+
 	//console.log(random);
 
 })();
